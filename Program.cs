400d7319e2201a67e70173d4685b87637a8b311d
pranavk36 <kpranav317@gmail.com>
2023-05-18 21:22:45 +0530

    Menu Created

--- Program.cs
+++ Program.cs
@@ -1,5 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-string Name(){
+﻿Console.WriteLine("Enter Name: ");
 string name = Console.ReadLine();
-return(name);
-}
+
+Console.WriteLine($"Hello {name}! We are learning C# basics /n");
+Console.WriteLine("/n");
+Console.WriteLine("This is a Math Game, To learn and understand Core C# Concepts /n");
+Console.WriteLine(@$"Please Select an Option: 
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quitting The Game");
+
+var GameSelected = Console.ReadLine();
+
+ switch(GameSelected){
+case  "a": 
+Additiongame();
+break;
+case  "s": 
+SubtractionGame();
+break;
+case  "m": 
+MultiplicationGame();
+break;
+case  "d": 
+DivisionGame();
+break;
+case  "q": 
+Console.WriteLine("Good Bye");
+Environment.Exit(1);
+break;
+ }
+
+ void Additiongame(){};
+ void SubtractionGame(){};
+ void MultiplicationGame(){};
+ void DivisionGame(){};
